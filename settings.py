5546ce47dd624911e90da6aeb2178b30df4594b9
Joel Christiansen <greenbadge.jc@gmail.com>
2015-03-04 22:30:57 -0700

    bugfixes. settings now works as of this commit

--- settings.py
+++ settings.py
@@ -6,18 +6,21 @@ c = conn.cursor()
 
 #get_settings :: String -> (String, String, Int, Int, Int)
 def get_settings(subreddit):
-    settings = c.execute("SELECT * FROM settings WHERE subreddit=?", subreddit)
+    settings = c.execute("SELECT * FROM settings WHERE subreddit=?", (subreddit,))
     return settings.fetchone()
 
-def change_settings(subreddit, first, days, duration, positions):
-    c.execute("INSERT OR REPLACE INTO settings VALUES ?", (subreddit, first, days, duration, positions))
+def change_settings(subreddit, first, frequency, duration, positions):
+    c.execute("UPDATE settings SET first=?, frequency=?, duration=?, positions=? WHERE subreddit=?", (first, frequency, duration, positions, subreddit))
     conn.commit()
 
-def remove_subreddit(subreddit):
-    c.execute("DELETE FROM settings WHERE subreddit=?", subreddit)
+def delete_subreddit(subreddit):
+    c.execute("DELETE FROM settings WHERE subreddit=?", (subreddit,))
     conn.commit()
 
 def get_all_settings():
     settings = c.execute("SELECT * FROM settings ORDER BY subreddit")
     return [i for i in settings]
 
+def add_subreddit(subreddit, first, frequency, duration, positions):
+    c.execute("INSERT INTO settings VALUES (?, ?, ?, ?, ?)", (subreddit, first, frequency, duration, positions))
+    conn.commit()
