f115cd4df004d68e506666fb4addefd428cba931
Joel Christiansen <greenbadge.jc@gmail.com>
2015-03-04 22:11:12 -0700

    comments explaining structure and specifying defaults where applicable. No code changes.

--- setup.py
+++ setup.py
@@ -1,19 +1,24 @@
 import sqlite3
 import json
 
+#------------------ DataBase for holding potential alterations in settings. Defaults are 
+#------------------    text      date  days  days   position
+#------------------ (subreddit,  first, 28,   7,     1)
 conn_set = sqlite3.connect("settings.db")
 c_set = conn_set.cursor()
 c_set.execute('''CREATE TABLE IF NOT EXISTS settings 
                 (subreddit text, first text, frequency integer, duration integer, positions integer)''')
 conn_set.commit()
 conn_set.close()
-#-------------------
 
+#------------------- JSON file (easier to hand modify) Defaults are 
+#------------------- 'subreddit': {'numberOfMods':1, 'mods':[modName], 'nominatedMods':[]}
 with open('modlist.json', 'w+') as modlist:
     modlist.write(json.dumps({"_structure":"subreddit:{'numberOfMods':0, 'mods':[], 'nominatedMods':[]}"}, 
                                 indent=4))
 
-#-------------------
+#------------------- DB again for holding all possible elections.
+#------------------- no need for defaults because there's nothing that could have a defualt
 conn_elect = sqlite3.connect('elections.db')
 c_elect = conn_elect.cursor()
 c_elect.execute('''CREATE TABLE IF NOT EXISTS elections
@@ -21,6 +26,7 @@ c_elect.execute('''CREATE TABLE IF NOT EXISTS elections
 conn_elect.commit()
 conn_elect.close()
 
+#------------------- Secrets are ment to be kept secret. 
 #-------------------
 with open('logins.json', 'w+') as logins:
     username = raw_input("Bot username: ")
